0b493be79423aca9602c800962cb8b906360a7a4
Lucas <lucas.bf@outlook.com>
2022-02-01 00:27:08 -0300

    adicao de dataset no teste

--- tests/FormatNameAuthorTest.php
+++ tests/FormatNameAuthorTest.php
@@ -6,30 +6,17 @@ test('Test Exception', function () {
     $formatNameService = new FormatNameAuthorService();
 
     expect(function() use ($formatNameService) {
-        $formatNameService->format(0, 'lucas');
-    })->toThrow(Exception::class);
-
-    expect(function() use ($formatNameService) {
-        $formatNameService->format(1, null);
+        $formatNameService->format(null);
     })->toThrow(Exception::class);
 })->group('name-author');
 
-test('Test Format One Name Author', function () {
-    $name = 'ferreira';
-
+test('Test Format Name Author', function ($name, $expect) {
     $formatNameService = new FormatNameAuthorService();
 
     $nameFormatted = $formatNameService->format($name);
 
-    expect('FERREIRA')->toBe($nameFormatted);
-})->group('name-author-format');
-
-test('Test Format Name Author Junior', function () {
-    $name = 'lucas ferreira junior';
-
-    $formatNameService = new FormatNameAuthorService();
-
-    $nameFormatted = $formatNameService->format($name);
-
-    expect($nameFormatted)->toBe('FERREIRA JUNIOR, Lucas');
-})->group('name-author-format');
+    expect($nameFormatted)->toBe($expect);
+})->with([
+    ['ferreira', 'FERREIRA'],
+    ['lucas ferreira junior', 'FERREIRA JUNIOR, Lucas']
+])->group('name-author');
