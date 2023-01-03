7cc5608e34e0838ba900242f86b6a4c92df7ea45
lucasbf1992 <lucas.bf@outlook.com>
2023-01-03 15:21:27 -0300

    remoção de classe

--- src/nameAuthors/main.php
+++
@@ -1,9 +0,0 @@
-<?php
-    echo "Iniciando a aplicação PHP\n";
-
-    echo "Digite a quantidade de nomes do autor\n";
-    $quantityNames = fgetc(STDIN);
-
-    echo "Digite o nome do autor\n";
-    $author = fgets(STDIN);
-
